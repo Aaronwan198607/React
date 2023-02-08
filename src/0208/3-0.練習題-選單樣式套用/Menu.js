3db5e48e9ee9a2eb913aa5035e78d380bb638403
”aaron” <helloaaron724@outlook.com>
2023-02-08 14:17:20 +0800

    選單樣式習題實作

--- src/0208/3-0.練習題-選單樣式套用/Menu.js
+++ src/0208/3-0.練習題-選單樣式套用/Menu.js
@@ -1,18 +1,28 @@
+import { useState } from 'react'
 import './Menu.css'
 
 function Menu() {
+  const [activeText, setActiveText] = useState('')
+  const menuItems=['首頁','關於我們','產品']
+
   return (
     <>
       <ul>
-        <li>
-          <a>首頁</a>
-        </li>
-        <li>
-          <a className="active">關於我們</a>
-        </li>
-        <li>
-          <a>產品</a>
-        </li>
+        {menuItems.map((v, i) => {
+          return (
+            <li key={i}>
+              <a
+                className={`${activeText === v ? 'active' : ''}`}
+                href="#/"
+                onClick={() => {
+                  setActiveText(v)
+                }}
+              >
+                {v}
+              </a>
+            </li>
+          )
+        })}
       </ul>
     </>
   )
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import Student from "./0206/4.jsx-map範例-2-json檔案導入/0206/3.map/Student";
-import List from './0208/1-2.練習題-物件陣列處理練習-提取11為api/List'
+import Menu from "./0208/3-0.練習題-選單樣式套用/Menu"
 
 function App() {
   return (
     <>
-      <List />
+      <Menu />
     </>
   )
 }
