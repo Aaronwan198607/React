309d679a19f0407f71a7ddd79dbd95e0cd2b6210
”aaron” <helloaaron724@outlook.com>
2023-02-10 13:35:00 +0800

    to-do操作介面練習

---
+++ src/0210/4-1.練習題-todo-操作介面/0210/3.todo/TodoApp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function TodoApp() {
+  return (
+    <>
+      <h1>待辨事項</h1>
+      <input type="text" />
+      <hr />
+      <ul>
+        <li>買牛奶</li>
+        <li>學react</li>
+      </ul>
+    </>
+  )
+}
+
+export default TodoApp
---
+++ src/0210/4-1.練習題-todo-操作介面/App.js
@@ -0,0 +1,11 @@
+import TodoApp from './0210/3.todo/TodoApp'
+
+function App() {
+  return (
+    <>
+      <TodoApp />
+    </>
+  )
+}
+
+export default App
